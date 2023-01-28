2abb0f8a694e44772d8ae4d47ab929b645ce4d9b
Kenneth Lauer <klauer@lodus.io>
2023-01-27 19:05:34 -0500

    Part 5 commit 3

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "odyssey-lift-off-part5-server-production-2a23.up.railway.app",
+  uri: "https://odyssey-lift-off-part5-server-production-2a23.up.railway.app",
   cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}> 
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
   </ApolloProvider>,
